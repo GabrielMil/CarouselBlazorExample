f1e89652dd5bef6695c5553e20501655928ed174
GabrielMil <milshteingabriel@gmail.com>
2022-04-07 01:21:37 +0300

    load

---
+++ wwwroot/js/startCarousel.js
@@ -0,0 +1,14 @@
+﻿export function initCarousel(){
+    $("#carousel").Cloud9Carousel( {
+        buttonLeft: $("#buttons > .left"),
+        buttonRight: $("#buttons > .right"),
+        autoPlay: false,
+        bringToFront: true,
+        onLoaded: function( carousel ) {
+            // Show carousel
+            $(carousel).css( 'visibility', 'visible' );
+            console.log( 'Carousel is ready!' );
+        },
+    } );
+    console.log('Initiated')
+}
